a3813a3a6431dde2a70cc9f6eb9f018434f7c016
1f10250139 <s1f102501394@iniad.org>
2026-07-16 11:53:20 +0900

    チェックボックスの修正２

--- todo/views.py
+++ todo/views.py
@@ -38,7 +38,10 @@ def close_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.completed = True
     task.save()
-    return redirect(request.META.get('HTTP_REFERER', 'detail', task_id=task.id))
+    referer = request.META.get('HTTP_REFERER')
+    if referer:
+        return redirect(referer)
+    return redirect('detail', task_id=task.id)
   
 def edit(request, task_id):
     task = get_object_or_404(Task, id=task_id)
